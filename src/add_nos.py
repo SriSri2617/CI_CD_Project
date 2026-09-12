33eb936154238867d8b43c2d8b0cf1a68454c018
#Mathumitha <cjmathu@gmail.com>
2026-09-12 21:58:51 +0200

    sample test

---
+++ src/add_nos.py
@@ -0,0 +1,2 @@
+def add_nos(a: int, b: int) -> int:
+    return a + b
---
+++ tests/unit_test/test_add_Nos.py
@@ -0,0 +1,7 @@
+import pytest
+from src.add_nos import add_nos
+
+@pytest.mark.unit
+def test_add_nos():
+    result = add_nos(29,30)
+    assert result == 59
